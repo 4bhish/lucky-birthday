eea76675edcaac347df152f8d77b9acec3a17a40
4bhish <108976136+4bhish@users.noreply.github.com>
2022-10-01 14:12:06 +0530

    Chore : funtions

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const inputDob = document.querySelector("#dob-input");
 const luckyNumber = document.querySelector("#lucky-number");
+const showMsg = document.querySelector(".show-result");
 
 const checkBtn = document.querySelector("#check-button");
 
-checkBtn.addEventListener("click", function clickHandler()
-{
-    
-})
 
-function luckyDob()
+checkBtn.addEventListener("click", function luckyDob()
 {
     const dob= inputDob.value;
     sum=calculate(dob)
 
-}
+    if(sum % luckyNumber.value === 0 )
+    {
+        showMessage("lmao")
+    }
+    else
+    {
+
+        
+        showMessage("az")
+    }
+
+
+})
+
 
 function calculate(dob)
 {
-    dob= dob.replaceAll("-","");
+    dob= dob.replaceAll("-","")
+    var sum=0;
+
+    for(i=0;i<dob.length;i++)
+    {
+        sum= sum + Number(dob.charAt(i))
+        
+    }
+    return sum;
+}
+function showMessage(msg)
+{
+    showMsg.innerText = msg
+
 }
